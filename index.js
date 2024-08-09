e45549f91d24082a77e2d2e6cdf36b3bdc665597
DannyK05 <olubanjok@gmail.com>
2024-08-09 21:25:25 +0100

    feat: improved the ui withimages an font, implemented responsiveness

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const question = document.getElementsByClassName("question")[0];
+const man = document.getElementsByClassName("english-man")[0];
+const woman = document.getElementsByClassName("spanish-woman")[0];
+const eye = document.getElementsByClassName("eyes")[0];
+const point = document.getElementsByClassName("point")[0];
 const english = document.getElementsByClassName("english")[0];
 const spanish = document.getElementsByClassName("spanish")[0];
 const englishBtn = document.getElementById("englishBtn");
@@ -10,13 +14,29 @@ function showEnglish() {
   english.style.display = "flex";
   spanish.style.display = "none";
   english.style.animation = "appear 1s";
+  man.style.display = "none";
+  man.style.animation = "disappear 1s";
+  woman.style.display = "none";
+  woman.style.animation = "disappear 1s";
+  eye.style.display = "appear 1s";
+  point.style.display = "appear 1s";
+  eye.style.display = "block";
+  point.style.display = "block";
 }
 
 function showSpanish() {
   question.style.display = "none";
   english.style.display = "none";
   spanish.style.display = "flex";
-  spanish.style.animation = "appear 1.5s";
+  spanish.style.animation = "appear 1s";
+  man.style.display = "none";
+  man.style.animation = "disappear 1s";
+  woman.style.display = "none";
+  woman.style.animation = "disappear 1s";
+  eye.style.display = "appear 1s";
+  point.style.display = "appear 1s";
+  eye.style.display = "block";
+  point.style.display = "block";
 }
 
 englishBtn.addEventListener("click", showEnglish);
